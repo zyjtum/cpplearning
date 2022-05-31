eb4078ee301bceafaad2bc62425909ab2fc7629a
zhiyuan <jizhiyuan829@gmail.com>
2022-05-31 20:25:37 +0200

    Add toupper and tolower

--- strings/stringfuns.cpp
+++ strings/stringfuns.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 int main(){
     string my_string = "the big brown dog";
-
+    /*
+    single quote is for char
+    double quote is for string
+    */
+    // toupper() and tolower() return ASCII code
+    //use char() to convert into char
     cout << char(toupper(my_string.at(0))) <<endl;
+
+    cout << char(toupper('d'))<<endl;
+
+    cout << char(tolower('F')) <<endl;
+
+    // char(my_string) is not allowed
 
 
     return 0;
